8332c46ec1e83cecd85a454a5d05006c203a3c3f
nhanta <46745074+nhanta@users.noreply.github.com>
2024-03-23 17:27:52 +0700

    Delete gatk copy directory

--- gatk copy/sra_to_fastq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Convert sra to fastq"
-for filename in sra/*
-do
-  echo $filename
-  fastq-dump --outdir fastq --gzip --skip-technical  --readids --dumpbase --split-3 --clip $filename
-done
